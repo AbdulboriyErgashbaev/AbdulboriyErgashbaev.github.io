b3b3f455231eecec84f57d173dc63d71e4491031
AbdulboriyErgashbaev <abdulboriy.ergashbaev2002@example.com>
2022-10-07 10:52:09 +0300

    media

--- src/components/DuaCard.js
+++ src/components/DuaCard.js
@@ -3,11 +3,13 @@ import "../StyleInScss/duaCard.css"
 function DuaCard(props) {
   return (
     <div className='card' >
-        <div className='right'><p> {props.zametka} </p></div>
+       <div className="card1">
+       <div className='right'><p> {props.zametka} </p></div>
         <h1>{props.name}</h1>
         <h3>{props.transcription}</h3>
         <audio src={props.audi} controls></audio>
         <h3>{props.translation}</h3>
+       </div>
     </div>
   )
 }
